fc1b15a47df0c0eda5770770263a585339fcab37
PanwarAshish <panwar.ashish10@gmail.com>
2022-08-09 19:43:44 +0530

    chore: react functional changes

--- Z2M-React/01-React-Basics/monsters-rolodex/src/App.js
+++ Z2M-React/01-React-Basics/monsters-rolodex/src/App.js
@@ -1,58 +1,46 @@
-import { Component } from "react";
+// import { Component } from "react";
+import { useState, useEffect } from "react";
+
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  onSearchChange = (event) => {
-    let searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return {
-        searchField,
-      };
-    });
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => {
-        this.setState(() => {
-          return { monsters: users, filteredMonsters: users };
-        });
-      });
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  render() {
-    const { monsters, searchField } = this.state;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchField);
     });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          className="search-box"
-          onChangeHandler={this.onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
-// function App() {}
+  const onSearchChange = (event) => {
+    let searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="search-box monster-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
--- Z2M-React/01-React-Basics/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ Z2M-React/01-React-Basics/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -3,17 +3,14 @@ import Card from "./card/card.component";
 
 import "./card-list.style.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => (
-          <Card monster={monster} />
-        ))}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => (
+        <Card monster={monster} />
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
