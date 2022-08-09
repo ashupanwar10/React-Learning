a6c1efdc6622bceaadc0afee64b705869cfe4c74
PanwarAshish <panwar.ashish10@gmail.com>
2022-08-09 13:17:40 +0530

    chore: initial commit on monsters app.

---
+++ Z2M-React/01-React-Basics/monsters-rolodex/src/App.js
@@ -0,0 +1,61 @@
+import { Component } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(() => {
+          return { monsters: users, filteredMonsters: users };
+        });
+      });
+  }
+
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            let searchField = event.target.value.toLowerCase();
+
+            this.setState(() => {
+              return {
+                searchField,
+              };
+            });
+          }}
+        />
+
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+// function App() {}
+
+export default App;
